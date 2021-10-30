868e47b59a7248987f324df75a507126c24d334e
devacr7 <ndevanandhss@gmail.com>
2021-10-30 20:53:24 +0530

    third commit

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,9 +1,11 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-const btn = document.getElementById("btn");
+const btn_start = document.getElementById("btn");
 const btn_stop = document.getElementById("btn-stop");
 const color = document.querySelector(".color");
 
-btn.addEventListener("click", function createHexCode() {
+btn_start.addEventListener("click", createHexCode());
+
+function createHexCode() {
     var hexCode = "#";
     var flag=true;
     for(let i=0;i<6;i++) {
@@ -11,13 +13,13 @@ btn.addEventListener("click", function createHexCode() {
     }
     console.log(hexCode);
     color.textContent = hexCode;
-    document.body.style.backgroundColor = hexCode;  
-});
-btn_stop.addEventListener("click", function() {
-    flag=false;
-});
-if(flag==true)
-    setInterval(function(){createHexCode();}, 3000);
+    document.body.style.backgroundColor = hexCode; 
+    btn_stop.addEventListener("click", function() {
+        return;
+    });
+    if(flag==true)
+        setInterval(function(){createHexCode();}, 3000); 
+}
 
 function getRandomNumber() {
     let currColorIdx = Math.floor(Math.random() * hex.length);
